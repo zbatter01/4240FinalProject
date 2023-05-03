f998afc2bcdaf3bdc60eca8ef97cc5e7c5b09993
Zachary Batterson <zbatter@clemson.edu>
2023-05-03 02:53:00 +0000

    Added packet sniffing with verbose option, plan to filter them down to IOT now

--- iotsniff.py
+++ iotsniff.py
@@ -2,3 +2,29 @@
 # Josiah Lamont
 # Konnor Mcdowell
 
+import argparse
+from scapy.all import *
+
+
+class Sniffer:
+	def __init__(self,args):
+		self.args = args
+	def __call__(self,packet):
+		if self.args.verbose:
+			packet.show()
+		else:
+			print(packet.summary())
+
+	def run_forever(self):
+		sniff(iface=self.args.interface, prn=self, store=0)
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-v','--verbose',default=False, action='store_true', help='verbose output, more information')
+
+	parser.add_argument('-i', '--interface', type=str, required=True, help='network interface name')
+	args = parser.parse_args()
+	sniffer = Sniffer(args)
+
+	sniffer.run_forever()
+
---
+++ makefile
@@ -0,0 +1,7 @@
+default:
+	python3 iotsniff.py -i wlp0s20f3
+verbose: 
+
+	python3 iotsniff.py -v -i wlp0s20f3
+open:
+	vi iotsniff.py
